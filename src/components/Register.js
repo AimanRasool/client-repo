a29f685cc1ea6155bc7221e2d4054d8aa269d877
AimanRasool <aimanrasool474@gmail.com>
2024-08-09 15:15:42 +0500

    added url

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:8000/api/register',
+      await axios.post('https://server-repo-tan.vercel.app/api/register',
          { username,
            email, 
            password });
