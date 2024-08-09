831bfdf92f045c967d61a3e8a2609380922b329e
AimanRasool <127721724+AimanRasool@users.noreply.github.com>
2024-08-09 15:37:37 +0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/api/login', { email, password });
+      const response = await axios.post('https://server-repo-tan.vercel.app/api/login', { email, password });
       localStorage.setItem('token', response.data.token);
       navigate('/home');
     } catch (err) {
